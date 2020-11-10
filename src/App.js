203ec7edf743a4c0d3e0f9c5d3f5bb8672673f8f
heri16 <527101+heri16@users.noreply.github.com>
2020-11-10 18:49:40 +0800

    Convert to Hook and Function Component

--- src/App.js
+++ src/App.js
@@ -1,90 +1,87 @@
-import React, { Component } from "react";
+import React, { useRef, useMemo, useState, useCallback } from "react";
 import "./input.css";
 
-const CODE_LENGTH = new Array(6).fill(0);
+const App = ({ codeLength = 6 }) => {
+    const CODE_LENGTH = useMemo(() => new Array(codeLength).fill(0), [codeLength]);
 
-class App extends Component {
-  input = React.createRef();
-  state = {
-    value: "",
-    focused: false,
-  };
+    const input = useRef(null);
+    const displayRects = useRef([]);
 
-  handleClick = e => {
-    const { index } = e.target.dataset;
-    if (index) {
-      this.setState(state => {
-        return {
-          value: state.value.slice(0, index),
-        };
-      });
-    }
-    this.input.current.focus();
-  };
-  handleFocus = () => {
-    this.setState({ focused: true });
-  };
-  handleBlur = () => {
-    this.setState({
-      focused: false,
-    });
-  };
-  handleKeyUp = e => {
-    if (e.key === "Backspace") {
-      this.setState(state => {
-        return {
-          value: state.value.slice(0, state.value.length - 1),
-        };
-      });
-    }
-  };
-  handleChange = e => {
-    const value = e.target.value;
+    const [value, setValue] = useState('');
+    const [focused, setFocused] = useState(false);
 
-    this.setState(state => {
-      if (state.value.length >= CODE_LENGTH.length) return null;
-      return {
-        value: (state.value + value).slice(0, CODE_LENGTH.length),
-      };
-    });
-  };
-  render() {
-    const { value, focused } = this.state;
+    const handleFocus = useCallback(() => setFocused(true), []);
+    const handleBlur = useCallback(() => setFocused(false), []);
+
+    const handleClick = useCallback(e => {
+      const { index } = e.target.dataset;
+      if (index) setValue(value.slice(0, index));
+      input.current.focus();
+    }, [value]);
+
+    const handleKeyUp = useCallback(e => {
+      if (e.key === "Backspace") {
+        setValue(value.slice(0, value.length - 1));
+      }
+    }, [value]);
+
+    const handleChange = useCallback(e => {
+      const newValue = e.target.value;
+      if (value.length < CODE_LENGTH.length) {
+        setValue((value + newValue).slice(0, CODE_LENGTH.length));
+      }
+    }, [value]);
+
+    const measuredRef = useCallback(node => {
+      if (node !== null) {
+        const { index } = node.dataset;
+        if (index) {
+          displayRects.current[index] = {
+            left: node.offsetLeft,
+            top: node.offsetTop,
+            width: node.offsetWidth,
+            height: node.offsetHeight,
+          };
+        }
+      }
+    }, []);
 
     const values = value.split("");
 
     const selectedIndex =
       values.length < CODE_LENGTH.length ? values.length : CODE_LENGTH.length - 1;
 
+    const selectedRect = displayRects.current[selectedIndex];
+
     const hideInput = !(values.length < CODE_LENGTH.length);
 
     return (
       <div className="App">
-        <div className="wrap" onClick={this.handleClick}>
+        <div className="wrap" onClick={handleClick}>
           <input
             type="tel"
             autoComplete="one-time-code"
             value=""
-            ref={this.input}
-            onChange={this.handleChange}
-            onKeyUp={this.handleKeyUp}
-            onFocus={this.handleFocus}
-            onBlur={this.handleBlur}
+            ref={input}
+            onChange={handleChange}
+            onKeyUp={handleKeyUp}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
             className="input"
             style={{
-              width: "32px",
-              top: "0px",
-              bottom: "0px",
-              left: `${selectedIndex * 32}px`,
+              left: `${selectedRect ? selectedRect.left : selectedIndex * 32}px`,
+              top: `${selectedRect ? selectedRect.top : 0}px`,
+              width: `${selectedRect ? selectedRect.width : 32}px`,
+              height: `${selectedRect ? selectedRect.height : 58}px`,
               opacity: hideInput ? 0 : 1,
             }}
           />
-          {CODE_LENGTH.map((v, index) => {
+          {CODE_LENGTH.map((_v, index) => {
             const selected = values.length === index;
             const filled = values.length === CODE_LENGTH.length && index === CODE_LENGTH.length - 1;
 
             return (
-              <div className="display" data-index={index} key={index}>
+              <div className="display" data-index={index} key={index} ref={measuredRef}>
                 {values[index]}
                 {(selected || filled) && focused && <div className="shadows" />}
               </div>
@@ -93,7 +90,6 @@ class App extends Component {
         </div>
       </div>
     );
-  }
 }
 
 export default App;
